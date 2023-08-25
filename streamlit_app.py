510352dd032b31009d5ec04b256668a8f5a2dc36
Krzysztof Kalkowski <krzysztof.kalkowski@snowflake.com>
2023-08-25 22:13:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to my personal Streamlit!
+# Welcome to my Streamlit!
 
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
